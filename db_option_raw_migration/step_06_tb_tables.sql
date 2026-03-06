a4c0c049135e659c90fbc1a5890de48ee09969e1
Daniel Robert Costa <35615346+drobcosta@users.noreply.github.com>
2026-03-06 09:59:05 -0300

    Update step_06_tb_tables.sql

--- db_option_raw_migration/step_06_tb_tables.sql
+++ db_option_raw_migration/step_06_tb_tables.sql
@@ -26,13 +26,13 @@ CREATE TABLE IF NOT EXISTS data_catalog.tb_tables
 ) TABLESPACE pg_default;
 
 COMMENT ON TABLE data_catalog.tb_tables IS 'Tabela responsável por armazenar os nomes das tabelas vinculadas aos schemas e aos bancos de dados que deverão ser registrados na camada bronze';
-COMMENT ON COLUMNS data_catalog.tb_tables.id IS 'Coluna PK da tabela. Seu conteúdo é um MD5 da coluna NAME';
-COMMENT ON COLUMNS data_catalog.tb_tables.tb_databases_id IS 'Coluna FK referência em data_catalog.tb_databases(id). Registra a qual database esta tabela pertence';
-COMMENT ON COLUMNS data_catalog.tb_tables.tb_schemas_id IS 'Coluna FK referência em data_catalog.tb_schemas(id). Registra a qual schema esta tabela pertence';
-COMMENT ON COLUMNS data_catalog.tb_tables.tb_status_id IS 'Coluna FK referência em data_catalog.tb_status(id). Registra o status do objeto (table)';
-COMMENT ON COLUMNS data_catalog.tb_tables.tb_payload_period_id IS 'Coluna FK referência em data_catalog.tb_payload_period(id). Registra qual a periodicidade da carga (payload) entre camada raw e bronze deverá acontecer';
-COMMENT ON COLUMNS data_catalog.tb_tables.name IS 'Coluna que registra o nome do objeto (table)';
-COMMENT ON COLUMNS data_catalog.tb_tables.description IS 'Coluna que registra a descrição do objeto (table). Esta coluna inicialmente é NULL. Esta coluna é obrigatória para que o objeto seja criado na camada bronze';
-COMMENT ON COLUMNS data_catalog.tb_tables.active IS 'Coluna que registra se o objeto (table) está ativo no catálogo';
-COMMENT ON COLUMNS data_catalog.tb_tables.created_at IS 'Coluna que registra o timestamp exato da inserção do registro na tabela';
-COMMENT ON COLUMNS data_catalog.tb_tables.updated_at IS 'Coluna que registra o timestamp exato da alteração da coluna tb_status_id do registro na tabela';
+COMMENT ON COLUMN data_catalog.tb_tables.id IS 'Coluna PK da tabela. Seu conteúdo é um MD5 da coluna NAME';
+COMMENT ON COLUMN data_catalog.tb_tables.tb_databases_id IS 'Coluna FK referência em data_catalog.tb_databases(id). Registra a qual database esta tabela pertence';
+COMMENT ON COLUMN data_catalog.tb_tables.tb_schemas_id IS 'Coluna FK referência em data_catalog.tb_schemas(id). Registra a qual schema esta tabela pertence';
+COMMENT ON COLUMN data_catalog.tb_tables.tb_status_id IS 'Coluna FK referência em data_catalog.tb_status(id). Registra o status do objeto (table)';
+COMMENT ON COLUMN data_catalog.tb_tables.tb_payload_period_id IS 'Coluna FK referência em data_catalog.tb_payload_period(id). Registra qual a periodicidade da carga (payload) entre camada raw e bronze deverá acontecer';
+COMMENT ON COLUMN data_catalog.tb_tables.name IS 'Coluna que registra o nome do objeto (table)';
+COMMENT ON COLUMN data_catalog.tb_tables.description IS 'Coluna que registra a descrição do objeto (table). Esta coluna inicialmente é NULL. Esta coluna é obrigatória para que o objeto seja criado na camada bronze';
+COMMENT ON COLUMN data_catalog.tb_tables.active IS 'Coluna que registra se o objeto (table) está ativo no catálogo';
+COMMENT ON COLUMN data_catalog.tb_tables.created_at IS 'Coluna que registra o timestamp exato da inserção do registro na tabela';
+COMMENT ON COLUMN data_catalog.tb_tables.updated_at IS 'Coluna que registra o timestamp exato da alteração da coluna tb_status_id do registro na tabela';
