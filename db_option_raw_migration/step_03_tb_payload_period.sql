391756d6e2ecad68097d03b4813eff4045fb5385
Daniel Robert Costa <35615346+drobcosta@users.noreply.github.com>
2026-03-09 12:18:56 -0300

    Fix comment syntax in SQL migration script

--- db_option_raw_migration/step_03_tb_payload_period.sql
+++ db_option_raw_migration/step_03_tb_payload_period.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS data_catalog.tb_payload_period
     CONSTRAINT tb_payload_period_pkey PRIMARY KEY (id)
 ) TABLESPACE pg_default;
 
-COMMENT ON TABLE data_catalog.tb_payload_period IS 'Tabela responsável pelos registros dos períodos de execução de carga (payload) das tabelas disponíveis na camada bronze. São períodos pré-configurados para que a carga de dados da camada raw para a camada bronze aconteça.'
+COMMENT ON TABLE data_catalog.tb_payload_period IS 'Tabela responsável pelos registros dos períodos de execução de carga (payload) das tabelas disponíveis na camada bronze. São períodos pré-configurados para que a carga de dados da camada raw para a camada bronze aconteça.';
 COMMENT ON COLUMN data_catalog.tb_payload_period.id IS 'Coluna PK da tabela';
 COMMENT ON COLUMN data_catalog.tb_payload_period.name IS 'Coluna que registra o nome do período de execução de carga (payload)';
 COMMENT ON COLUMN data_catalog.tb_payload_period.minutes IS 'Coluna que registra a quantidade de minutos do período de execução de carga';
