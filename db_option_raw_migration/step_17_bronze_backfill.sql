a104ede27b7c9a76aa5070ef445fee68fb54e08d
Daniel Robert Costa <35615346+drobcosta@users.noreply.github.com>
2026-03-25 11:39:07 -0300

    Create view vw_bronze_backfill for bronze layer data
    Added a new view 'vw_bronze_backfill' to select distinct database, schema, and table information with backfill control data.

--- db_option_raw_migration/step_17_bronze_backfill.sql
+++ db_option_raw_migration/step_17_bronze_backfill.sql
@@ -1438,3 +1438,26 @@ BEGIN
 		RETURN;
 END; 
 $BODY$;
+
+-- View: data_catalog.vw_bronze_backfill
+
+-- DROP VIEW data_catalog.vw_bronze_backfill;
+
+CREATE OR REPLACE VIEW data_catalog.vw_bronze_backfill
+ AS
+ SELECT DISTINCT vw.database_id,
+    vw.database_name,
+    vw.schema_id,
+    vw.schema_name,
+    vw.table_id,
+    vw.table_name,
+    bbc.insert_timestamp,
+    bbc.insert_done,
+    bbc.update_timestamp,
+    bbc.update_done,
+    bbc.delete_timestamp,
+    bbc.delete_done
+   FROM data_catalog.vw_catalog vw
+     LEFT JOIN data_catalog.bronze_layer_control blc ON blc.tb_databases_id::text = vw.database_id::text AND blc.tb_schemas_id::text = vw.schema_id::text AND blc.tb_tables_id::text = vw.table_id::text
+     LEFT JOIN data_catalog.bronze_backfill_control bbc ON bbc.tb_databases_id::text = vw.database_id::text AND bbc.tb_schemas_id::text = vw.schema_id::text AND bbc.tb_tables_id::text = vw.table_id::text AND bbc.tb_columns_id IS NULL
+  WHERE vw.database_status_id = 5 AND vw.schema_status_id = 5 AND vw.table_status_id = 5 AND blc.tb_tables_id IS NULL AND (bbc.id IS NULL OR bbc.insert_done IS FALSE OR bbc.update_done IS FALSE OR bbc.delete_done IS FALSE);
