94672ff1b74efd5f67fdd5db8c75e136038e0f67
Daniel Robert Costa <35615346+drobcosta@users.noreply.github.com>
2026-03-25 12:16:31 -0300

    Modify bronze_layer_control table schema
    Replaced start_timestamp with target_timestamp in the bronze_layer_control table.

--- db_option_raw_migration/step_16_bronze_layer_control.sql
+++ db_option_raw_migration/step_16_bronze_layer_control.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS data_catalog.bronze_layer_control (
 	tb_databases_id VARCHAR(32) NOT NULL,
 	tb_schemas_id VARCHAR(32) NOT NULL,
 	tb_tables_id VARCHAR(32) NOT NULL,
-	start_timestamp TIMESTAMP WITHOUT TIME ZONE,
+	target_timestamp TIMESTAMP WITHOUT TIME ZONE,
 	insert_qty BIGINT,
 	update_qty BIGINT,
 	delete_qty BIGINT,
@@ -14,7 +14,7 @@ COMMENT ON TABLE data_catalog.bronze_layer_control IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.tb_databases_id IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.tb_schemas_id IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.tb_tables_id IS '';
-COMMENT ON COLUMN data_catalog.bronze_layer_control.start_timestamp IS '';
+COMMENT ON COLUMN data_catalog.bronze_layer_control.target_timestamp IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.insert_qty IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.update_qty IS '';
 COMMENT ON COLUMN data_catalog.bronze_layer_control.delete_qty IS '';
